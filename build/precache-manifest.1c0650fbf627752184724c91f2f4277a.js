8d7506f764a6d694dcea971cbb56a1dfcfa33268
pat-white-code <pat.k.white@gmail.com>
2020-05-14 11:10:00 -0500

    removes build/ from gitignore

---
+++ build/precache-manifest.1c0650fbf627752184724c91f2f4277a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68eec22fea3b9859b78575514a27200",
+    "url": "./index.html"
+  },
+  {
+    "revision": "602ad323824cc70f4429",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce15d59c9f0f0d511256",
+    "url": "./static/js/2.097d671a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.097d671a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602ad323824cc70f4429",
+    "url": "./static/js/main.1675ff25.chunk.js"
+  },
+  {
+    "revision": "a1a3ca351d3c5d4a976f",
+    "url": "./static/js/runtime-main.51ae4deb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
